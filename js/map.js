3b2471a0359b6d8edfc6f5a7db13ceb7fa1999ae
ahmetmerkit <ahmetmerkitt@gmail.com>
2025-02-16 16:05:34 +0300

    Updated project with latest changes

--- js/map.js
+++ js/map.js
@@ -1,10 +1,12 @@
 function myMap() {
   var mapCanvas = document.getElementById("map");
-  var myCenter = new google.maps.LatLng(45.579591,9.164906); 
-  var mapOptions = {center: myCenter, zoom: 13};
-  var map = new google.maps.Map(mapCanvas,mapOptions);
+  var myCenter = new google.maps.LatLng(41.0888118, 29.0567905); 
+  var mapOptions = {center: myCenter, zoom: 15}; // Increased zoom for better focus
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  
   var marker = new google.maps.Marker({
     position: myCenter,
+    map: map,
+    title: "Fani Bosphorus Restaurant" // Added title when hovering
   });
-  marker.setMap(map);
 }
